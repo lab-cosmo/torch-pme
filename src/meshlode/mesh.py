2df5f0d0a3a5616c5d75bbee2b95b288b8c91a58
Philip Loche <ploche@physik.fu-berlin.de>
2023-08-31 13:35:49 +0200

    fix lint and consistently update to metatensor

--- src/meshlode/mesh.py
+++ src/meshlode/mesh.py
@@ -1,21 +1,22 @@
-import torch
-
 from typing import Optional
+
+import torch
 from metatensor.torch import TensorBlock
+
 from .system import System
+
 
 class FieldBuilder(torch.nn.Module):
     def __init__(self):
         pass
-    
-    def compute(self, 
-                system : System,
-                embeddings: Optional[TensorBlock] = None
-               ):
+
+    def compute(self, system: System, embeddings: Optional[TensorBlock] = None):
         pass
-    
+
+
 class MeshInterpolate(torch.nn.Module):
     pass
+
 
 class FieldProjector(torch.nn.Module):
     pass
