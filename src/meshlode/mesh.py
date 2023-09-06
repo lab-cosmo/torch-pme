6864ef497fe3b3e69bdaf132e85df59ed4cddfac
Michele Ceriotti <michele.ceriotti@gmail.com>
2023-09-06 05:25:13 -0700

    Fixed more bugs (my fault) in the mesh code

--- src/meshlode/mesh.py
+++ src/meshlode/mesh.py
@@ -76,7 +76,7 @@ class FieldBuilder(torch.nn.Module):
         # TODO - THIS IS COPIED AND JUST ADAPTED FROM M&k CODE. NEEDS CLEANUP AND COMMENTING (AS WELL AS COPYING OVER HIGHER P AND HANDLING OF PBC)
         positions_cell = torch.div(system.positions, mesh.spacing)
         positions_cell_idx = torch.round(positions_cell).long()
-                
+        
         if self.mesh_interpolation_order == 2:
             # TODO - CHECK IF THIS ACTUALLY WORKS, GETTING FISHY RESULTS
             l_dist = positions_cell - positions_cell_idx
@@ -94,7 +94,7 @@ class FieldBuilder(torch.nn.Module):
             frac_111 = r_dist[:, 0] * r_dist[:, 1] * r_dist[:, 2]        
 
             rp_a_species = positions_cell_idx
-            print(rp_a_species.shape, embeddings.shape, frac_000.shape, w[:, (rp_a_species[:,0]+0)% N_mesh, (rp_a_species[:,1]+0)% N_mesh, (rp_a_species[:,2]+0) % N_mesh].shape)
+            
             # Perform actual smearing on density grid. takes indices modulo N_mesh to handle PBC
             w[:, (rp_a_species[:,0]+0)% N_mesh, (rp_a_species[:,1]+0)% N_mesh, (rp_a_species[:,2]+0) % N_mesh] += frac_000*embeddings.T
             w[:, (rp_a_species[:,0]+0)% N_mesh, (rp_a_species[:,1]+0)% N_mesh, (rp_a_species[:,2]+1) % N_mesh] += frac_001*embeddings.T
@@ -110,79 +110,79 @@ class FieldBuilder(torch.nn.Module):
             w = mesh.values
             N_mesh = mesh.n_mesh
             # Define auxilary functions
-            f_m = lambda x: (1 - (x+x))**2/8
+            f_m = lambda x: ((x+x)-1)**2/8
             f_0 = lambda x: (3/4 - x*x)
-            f_1 = lambda x: (1 + (x+x))**2/8
+            f_p = lambda x: ((x+x)+1)**2/8
             weight_m = f_m(dist)
             weight_0 = f_0(dist)
-            weight_1 = f_1(dist)
+            weight_p = f_p(dist)
 
             frac_mmm = weight_m[:,0] * weight_m[:,1] * weight_m[:,2]
             frac_mm0 = weight_m[:,0] * weight_m[:,1] * weight_0[:,2]
-            frac_mm1 = weight_m[:,0] * weight_m[:,1] * weight_1[:,2]
+            frac_mmp = weight_m[:,0] * weight_m[:,1] * weight_p[:,2]
             frac_m0m = weight_m[:,0] * weight_0[:,1] * weight_m[:,2]
             frac_m00 = weight_m[:,0] * weight_0[:,1] * weight_0[:,2]
-            frac_m01 = weight_m[:,0] * weight_0[:,1] * weight_1[:,2]
-            frac_m1m = weight_m[:,0] * weight_1[:,1] * weight_m[:,2]
-            frac_m10 = weight_m[:,0] * weight_1[:,1] * weight_0[:,2]
-            frac_m11 = weight_m[:,0] * weight_1[:,1] * weight_1[:,2]
+            frac_m0p = weight_m[:,0] * weight_0[:,1] * weight_p[:,2]
+            frac_mpm = weight_m[:,0] * weight_p[:,1] * weight_m[:,2]
+            frac_mp0 = weight_m[:,0] * weight_p[:,1] * weight_0[:,2]
+            frac_mpp = weight_m[:,0] * weight_p[:,1] * weight_p[:,2]
 
             frac_0mm = weight_0[:,0] * weight_m[:,1] * weight_m[:,2]
             frac_0m0 = weight_0[:,0] * weight_m[:,1] * weight_0[:,2]
-            frac_0m1 = weight_0[:,0] * weight_m[:,1] * weight_1[:,2]
+            frac_0mp = weight_0[:,0] * weight_m[:,1] * weight_p[:,2]
             frac_00m = weight_0[:,0] * weight_0[:,1] * weight_m[:,2]
             frac_000 = weight_0[:,0] * weight_0[:,1] * weight_0[:,2]
-            frac_001 = weight_0[:,0] * weight_0[:,1] * weight_1[:,2]
-            frac_01m = weight_0[:,0] * weight_1[:,1] * weight_m[:,2]
-            frac_010 = weight_0[:,0] * weight_1[:,1] * weight_0[:,2]
-            frac_011 = weight_0[:,0] * weight_1[:,1] * weight_1[:,2]
+            frac_00p = weight_0[:,0] * weight_0[:,1] * weight_p[:,2]
+            frac_0pm = weight_0[:,0] * weight_p[:,1] * weight_m[:,2]
+            frac_0p0 = weight_0[:,0] * weight_p[:,1] * weight_0[:,2]
+            frac_0pp = weight_0[:,0] * weight_p[:,1] * weight_p[:,2]
 
-            frac_1mm = weight_1[:,0] * weight_m[:,1] * weight_m[:,2]
-            frac_1m0 = weight_1[:,0] * weight_m[:,1] * weight_0[:,2]
-            frac_1m1 = weight_1[:,0] * weight_m[:,1] * weight_1[:,2]
-            frac_10m = weight_1[:,0] * weight_0[:,1] * weight_m[:,2]
-            frac_100 = weight_1[:,0] * weight_0[:,1] * weight_0[:,2]
-            frac_101 = weight_1[:,0] * weight_0[:,1] * weight_1[:,2]
-            frac_11m = weight_1[:,0] * weight_1[:,1] * weight_m[:,2]
-            frac_110 = weight_1[:,0] * weight_1[:,1] * weight_0[:,2]
-            frac_111 = weight_1[:,0] * weight_1[:,1] * weight_1[:,2]            
+            frac_pmm = weight_p[:,0] * weight_m[:,1] * weight_m[:,2]
+            frac_pm0 = weight_p[:,0] * weight_m[:,1] * weight_0[:,2]
+            frac_pmp = weight_p[:,0] * weight_m[:,1] * weight_p[:,2]
+            frac_p0m = weight_p[:,0] * weight_0[:,1] * weight_m[:,2]
+            frac_p00 = weight_p[:,0] * weight_0[:,1] * weight_0[:,2]
+            frac_p0p = weight_p[:,0] * weight_0[:,1] * weight_p[:,2]
+            frac_ppm = weight_p[:,0] * weight_p[:,1] * weight_m[:,2]
+            frac_pp0 = weight_p[:,0] * weight_p[:,1] * weight_0[:,2]
+            frac_ppp = weight_p[:,0] * weight_p[:,1] * weight_p[:,2]            
    
-            rp_a_species = positions_cell_idx
-            w[:, (rp_a_species[:,0]+0)% N_mesh, (rp_a_species[:,1]+0)% N_mesh, (rp_a_species[:,2]+0) % N_mesh] += frac_000*embeddings.T
-            w[:, (rp_a_species[:,0]+1)% N_mesh, (rp_a_species[:,1]+0)% N_mesh, (rp_a_species[:,2]+0) % N_mesh] += frac_001*embeddings.T
-            w[:, (rp_a_species[:,0]-1)% N_mesh, (rp_a_species[:,1]+0)% N_mesh, (rp_a_species[:,2]+0) % N_mesh] += frac_00m*embeddings.T
+            pci = positions_cell_idx
+            w[:, (pci[:,2]+0)% N_mesh, (pci[:,1]+0)% N_mesh, (pci[:,0]+0) % N_mesh] += frac_000*embeddings.T
+            w[:, (pci[:,2]+1)% N_mesh, (pci[:,1]+0)% N_mesh, (pci[:,0]+0) % N_mesh] += frac_00p*embeddings.T
+            w[:, (pci[:,2]-1)% N_mesh, (pci[:,1]+0)% N_mesh, (pci[:,0]+0) % N_mesh] += frac_00m*embeddings.T
             
-            w[:, (rp_a_species[:,0]+0)% N_mesh, (rp_a_species[:,1]+1)% N_mesh, (rp_a_species[:,2]+0) % N_mesh] += frac_010*embeddings.T
-            w[:, (rp_a_species[:,0]+1)% N_mesh, (rp_a_species[:,1]+1)% N_mesh, (rp_a_species[:,2]+0) % N_mesh] += frac_011*embeddings.T
-            w[:, (rp_a_species[:,0]-1)% N_mesh, (rp_a_species[:,1]+1)% N_mesh, (rp_a_species[:,2]+0) % N_mesh] += frac_01m*embeddings.T
+            w[:, (pci[:,2]+0)% N_mesh, (pci[:,1]+1)% N_mesh, (pci[:,0]+0) % N_mesh] += frac_0p0*embeddings.T
+            w[:, (pci[:,2]+1)% N_mesh, (pci[:,1]+1)% N_mesh, (pci[:,0]+0) % N_mesh] += frac_0pp*embeddings.T
+            w[:, (pci[:,2]-1)% N_mesh, (pci[:,1]+1)% N_mesh, (pci[:,0]+0) % N_mesh] += frac_0pm*embeddings.T
             
-            w[:, (rp_a_species[:,0]+0)% N_mesh, (rp_a_species[:,1]-1)% N_mesh, (rp_a_species[:,2]+0) % N_mesh] += frac_0m0*embeddings.T
-            w[:, (rp_a_species[:,0]+1)% N_mesh, (rp_a_species[:,1]-1)% N_mesh, (rp_a_species[:,2]+0) % N_mesh] += frac_0m1*embeddings.T
-            w[:, (rp_a_species[:,0]-1)% N_mesh, (rp_a_species[:,1]-1)% N_mesh, (rp_a_species[:,2]+0) % N_mesh] += frac_0mm*embeddings.T
+            w[:, (pci[:,2]+0)% N_mesh, (pci[:,1]-1)% N_mesh, (pci[:,0]+0) % N_mesh] += frac_0m0*embeddings.T
+            w[:, (pci[:,2]+1)% N_mesh, (pci[:,1]-1)% N_mesh, (pci[:,0]+0) % N_mesh] += frac_0mp*embeddings.T
+            w[:, (pci[:,2]-1)% N_mesh, (pci[:,1]-1)% N_mesh, (pci[:,0]+0) % N_mesh] += frac_0mm*embeddings.T
             
-            w[:, (rp_a_species[:,0]+0)% N_mesh, (rp_a_species[:,1]+0)% N_mesh, (rp_a_species[:,2]+1) % N_mesh] += frac_100*embeddings.T
-            w[:, (rp_a_species[:,0]+1)% N_mesh, (rp_a_species[:,1]+0)% N_mesh, (rp_a_species[:,2]+1) % N_mesh] += frac_101*embeddings.T
-            w[:, (rp_a_species[:,0]-1)% N_mesh, (rp_a_species[:,1]+0)% N_mesh, (rp_a_species[:,2]+1) % N_mesh] += frac_10m*embeddings.T
+            w[:, (pci[:,2]+0)% N_mesh, (pci[:,1]+0)% N_mesh, (pci[:,0]+1) % N_mesh] += frac_p00*embeddings.T
+            w[:, (pci[:,2]+1)% N_mesh, (pci[:,1]+0)% N_mesh, (pci[:,0]+1) % N_mesh] += frac_p0p*embeddings.T
+            w[:, (pci[:,2]-1)% N_mesh, (pci[:,1]+0)% N_mesh, (pci[:,0]+1) % N_mesh] += frac_p0m*embeddings.T
             
-            w[:, (rp_a_species[:,0]+0)% N_mesh, (rp_a_species[:,1]+1)% N_mesh, (rp_a_species[:,2]+1) % N_mesh] += frac_110*embeddings.T
-            w[:, (rp_a_species[:,0]+1)% N_mesh, (rp_a_species[:,1]+1)% N_mesh, (rp_a_species[:,2]+1) % N_mesh] += frac_111*embeddings.T
-            w[:, (rp_a_species[:,0]-1)% N_mesh, (rp_a_species[:,1]+1)% N_mesh, (rp_a_species[:,2]+1) % N_mesh] += frac_11m*embeddings.T
+            w[:, (pci[:,2]+0)% N_mesh, (pci[:,1]+1)% N_mesh, (pci[:,0]+1) % N_mesh] += frac_pp0*embeddings.T
+            w[:, (pci[:,2]+1)% N_mesh, (pci[:,1]+1)% N_mesh, (pci[:,0]+1) % N_mesh] += frac_ppp*embeddings.T
+            w[:, (pci[:,2]-1)% N_mesh, (pci[:,1]+1)% N_mesh, (pci[:,0]+1) % N_mesh] += frac_ppm*embeddings.T
             
-            w[:, (rp_a_species[:,0]+0)% N_mesh, (rp_a_species[:,1]-1)% N_mesh, (rp_a_species[:,2]+1) % N_mesh] += frac_1m0*embeddings.T
-            w[:, (rp_a_species[:,0]+1)% N_mesh, (rp_a_species[:,1]-1)% N_mesh, (rp_a_species[:,2]+1) % N_mesh] += frac_1m1*embeddings.T
-            w[:, (rp_a_species[:,0]-1)% N_mesh, (rp_a_species[:,1]-1)% N_mesh, (rp_a_species[:,2]+1) % N_mesh] += frac_1mm*embeddings.T
+            w[:, (pci[:,2]+0)% N_mesh, (pci[:,1]-1)% N_mesh, (pci[:,0]+1) % N_mesh] += frac_pm0*embeddings.T
+            w[:, (pci[:,2]+1)% N_mesh, (pci[:,1]-1)% N_mesh, (pci[:,0]+1) % N_mesh] += frac_pmp*embeddings.T
+            w[:, (pci[:,2]-1)% N_mesh, (pci[:,1]-1)% N_mesh, (pci[:,0]+1) % N_mesh] += frac_pmm*embeddings.T
 
-            w[:, (rp_a_species[:,0]+0)% N_mesh, (rp_a_species[:,1]+0)% N_mesh, (rp_a_species[:,2]-1) % N_mesh] += frac_m00*embeddings.T
-            w[:, (rp_a_species[:,0]+1)% N_mesh, (rp_a_species[:,1]+0)% N_mesh, (rp_a_species[:,2]-1) % N_mesh] += frac_m01*embeddings.T
-            w[:, (rp_a_species[:,0]-1)% N_mesh, (rp_a_species[:,1]+0)% N_mesh, (rp_a_species[:,2]-1) % N_mesh] += frac_m0m*embeddings.T
+            w[:, (pci[:,2]+0)% N_mesh, (pci[:,1]+0)% N_mesh, (pci[:,0]-1) % N_mesh] += frac_m00*embeddings.T
+            w[:, (pci[:,2]+1)% N_mesh, (pci[:,1]+0)% N_mesh, (pci[:,0]-1) % N_mesh] += frac_m0p*embeddings.T
+            w[:, (pci[:,2]-1)% N_mesh, (pci[:,1]+0)% N_mesh, (pci[:,0]-1) % N_mesh] += frac_m0m*embeddings.T
             
-            w[:, (rp_a_species[:,0]+0)% N_mesh, (rp_a_species[:,1]+1)% N_mesh, (rp_a_species[:,2]-1) % N_mesh] += frac_m10*embeddings.T
-            w[:, (rp_a_species[:,0]+1)% N_mesh, (rp_a_species[:,1]+1)% N_mesh, (rp_a_species[:,2]-1) % N_mesh] += frac_m11*embeddings.T
-            w[:, (rp_a_species[:,0]-1)% N_mesh, (rp_a_species[:,1]+1)% N_mesh, (rp_a_species[:,2]-1) % N_mesh] += frac_m1m*embeddings.T
+            w[:, (pci[:,2]+0)% N_mesh, (pci[:,1]+1)% N_mesh, (pci[:,0]-1) % N_mesh] += frac_mp0*embeddings.T
+            w[:, (pci[:,2]+1)% N_mesh, (pci[:,1]+1)% N_mesh, (pci[:,0]-1) % N_mesh] += frac_mpp*embeddings.T
+            w[:, (pci[:,2]-1)% N_mesh, (pci[:,1]+1)% N_mesh, (pci[:,0]-1) % N_mesh] += frac_mpm*embeddings.T
             
-            w[:, (rp_a_species[:,0]+0)% N_mesh, (rp_a_species[:,1]-1)% N_mesh, (rp_a_species[:,2]-1) % N_mesh] += frac_mm0*embeddings.T
-            w[:, (rp_a_species[:,0]+1)% N_mesh, (rp_a_species[:,1]-1)% N_mesh, (rp_a_species[:,2]-1) % N_mesh] += frac_mm1*embeddings.T
-            w[:, (rp_a_species[:,0]-1)% N_mesh, (rp_a_species[:,1]-1)% N_mesh, (rp_a_species[:,2]-1) % N_mesh] += frac_mmm*embeddings.T
+            w[:, (pci[:,2]+0)% N_mesh, (pci[:,1]-1)% N_mesh, (pci[:,0]-1) % N_mesh] += frac_mm0*embeddings.T
+            w[:, (pci[:,2]+1)% N_mesh, (pci[:,1]-1)% N_mesh, (pci[:,0]-1) % N_mesh] += frac_mmp*embeddings.T
+            w[:, (pci[:,2]-1)% N_mesh, (pci[:,1]-1)% N_mesh, (pci[:,0]-1) % N_mesh] += frac_mmm*embeddings.T
 
 
         mesh.values /= mesh.spacing**3
@@ -223,7 +223,7 @@ class MeshInterpolator(torch.nn.Module):
         # TODO rewrite the code below to use the more descriptive variables
         rp = points_cell_idx
         rp_0 = (points_cell_idx + 0) % mesh.n_mesh
-        rp_1 = (points_cell_idx + 1) % mesh.n_mesh
+        rp_p = (points_cell_idx + 1) % mesh.n_mesh
         rp_m = (points_cell_idx - 1 + mesh.n_mesh) % mesh.n_mesh
 
         interpolated_values = torch.zeros((points.shape[0], mesh.n_channels), 
@@ -233,49 +233,49 @@ class MeshInterpolator(torch.nn.Module):
             dist = points_cell - rp
 
             # Define auxilary functions
-            f_m = lambda x: (1 - (x+x))**2/8
+            f_m = lambda x: ((x+x)-1)**2/8
             f_0 = lambda x: (3/4 - x*x)
-            f_1 = lambda x: (1 + (x+x))**2/8
+            f_p = lambda x: ((x+x)+1)**2/8
             weight_m = f_m(dist)
             weight_0 = f_0(dist)
-            weight_1 = f_1(dist)
+            weight_p = f_p(dist)
 
             frac_mmm = weight_m[:,0] * weight_m[:,1] * weight_m[:,2]
             frac_mm0 = weight_m[:,0] * weight_m[:,1] * weight_0[:,2]
-            frac_mm1 = weight_m[:,0] * weight_m[:,1] * weight_1[:,2]
+            frac_mmp = weight_m[:,0] * weight_m[:,1] * weight_p[:,2]
             frac_m0m = weight_m[:,0] * weight_0[:,1] * weight_m[:,2]
             frac_m00 = weight_m[:,0] * weight_0[:,1] * weight_0[:,2]
-            frac_m01 = weight_m[:,0] * weight_0[:,1] * weight_1[:,2]
-            frac_m1m = weight_m[:,0] * weight_1[:,1] * weight_m[:,2]
-            frac_m10 = weight_m[:,0] * weight_1[:,1] * weight_0[:,2]
-            frac_m11 = weight_m[:,0] * weight_1[:,1] * weight_1[:,2]
+            frac_m0p = weight_m[:,0] * weight_0[:,1] * weight_p[:,2]
+            frac_mpm = weight_m[:,0] * weight_p[:,1] * weight_m[:,2]
+            frac_mp0 = weight_m[:,0] * weight_p[:,1] * weight_0[:,2]
+            frac_mpp = weight_m[:,0] * weight_p[:,1] * weight_p[:,2]
 
             frac_0mm = weight_0[:,0] * weight_m[:,1] * weight_m[:,2]
             frac_0m0 = weight_0[:,0] * weight_m[:,1] * weight_0[:,2]
-            frac_0m1 = weight_0[:,0] * weight_m[:,1] * weight_1[:,2]
+            frac_0mp = weight_0[:,0] * weight_m[:,1] * weight_p[:,2]
             frac_00m = weight_0[:,0] * weight_0[:,1] * weight_m[:,2]
             frac_000 = weight_0[:,0] * weight_0[:,1] * weight_0[:,2]
-            frac_001 = weight_0[:,0] * weight_0[:,1] * weight_1[:,2]
-            frac_01m = weight_0[:,0] * weight_1[:,1] * weight_m[:,2]
-            frac_010 = weight_0[:,0] * weight_1[:,1] * weight_0[:,2]
-            frac_011 = weight_0[:,0] * weight_1[:,1] * weight_1[:,2]
+            frac_00p = weight_0[:,0] * weight_0[:,1] * weight_p[:,2]
+            frac_0pm = weight_0[:,0] * weight_p[:,1] * weight_m[:,2]
+            frac_0p0 = weight_0[:,0] * weight_p[:,1] * weight_0[:,2]
+            frac_0pp = weight_0[:,0] * weight_p[:,1] * weight_p[:,2]
 
-            frac_1mm = weight_1[:,0] * weight_m[:,1] * weight_m[:,2]
-            frac_1m0 = weight_1[:,0] * weight_m[:,1] * weight_0[:,2]
-            frac_1m1 = weight_1[:,0] * weight_m[:,1] * weight_1[:,2]
-            frac_10m = weight_1[:,0] * weight_0[:,1] * weight_m[:,2]
-            frac_100 = weight_1[:,0] * weight_0[:,1] * weight_0[:,2]
-            frac_101 = weight_1[:,0] * weight_0[:,1] * weight_1[:,2]
-            frac_11m = weight_1[:,0] * weight_1[:,1] * weight_m[:,2]
-            frac_110 = weight_1[:,0] * weight_1[:,1] * weight_0[:,2]
-            frac_111 = weight_1[:,0] * weight_1[:,1] * weight_1[:,2]
+            frac_pmm = weight_p[:,0] * weight_m[:,1] * weight_m[:,2]
+            frac_pm0 = weight_p[:,0] * weight_m[:,1] * weight_0[:,2]
+            frac_pmp = weight_p[:,0] * weight_m[:,1] * weight_p[:,2]
+            frac_p0m = weight_p[:,0] * weight_0[:,1] * weight_m[:,2]
+            frac_p00 = weight_p[:,0] * weight_0[:,1] * weight_0[:,2]
+            frac_p0p = weight_p[:,0] * weight_0[:,1] * weight_p[:,2]
+            frac_ppm = weight_p[:,0] * weight_p[:,1] * weight_m[:,2]
+            frac_pp0 = weight_p[:,0] * weight_p[:,1] * weight_0[:,2]
+            frac_ppp = weight_p[:,0] * weight_p[:,1] * weight_p[:,2]
 
             for a in range(mesh.n_channels):
                 # TODO I think the calculation of the channels can be serialized
                 # Add up contributions to the potential from 27 closest mesh poitns
-                for x in ['m', '0', '1']:
-                    for y in ['m', '0', '1']:
-                        for z in ['m', '0', '1']:
+                for x in ['m', '0', 'p']:
+                    for y in ['m', '0', 'p']:
+                        for z in ['m', '0', 'p']:
                             # TODO write this out
                             command = f"""interpolated_values[:,a] += (
                             mesh.values[a, rp_{x}[:,0], rp_{y}[:,1], rp_{z}[:,2]] 
