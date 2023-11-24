8ebd1fb31cfe4786dd674c0739f8c1c03158036b
Kevin Kazuki Huguenin-Dumittan <kvhuguenin@gmail.com>
2023-11-24 17:09:35 +0100

    Add Mesh Potential calc. (single frame)

--- src/meshlode/calculators.py
+++ src/meshlode/calculators.py
@@ -14,6 +14,8 @@ from typing import List, Optional, Union
 import torch
 from metatensor.torch import Labels, TensorBlock, TensorMap
 
+from meshlode.mesh_interpolator import MeshInterpolator
+from meshlode.fourier_convolution import FourierSpaceConvolution
 from .system import System
 
 
@@ -84,3 +86,82 @@ class MeshPotential(torch.nn.Module):
     ) -> TensorMap:
         """forward just calls :py:meth:`CalculatorModule.compute`"""
         return self.compute(systems=systems, gradients=gradients)
+    
+    def _compute_single_frame(self, cell: torch.tensor,
+                              positions: torch.tensor, charges: torch.tensor,
+                              subtract_self=False) -> torch.tensor:
+        """
+        Compute the "electrostatic" potential at the position of all atoms in a
+        structure.
+
+        :param cell: torch.tensor of shape (3,3). Describes the unit cell of the structure,
+        where cell[i] is the i-th basis vector.
+        :param positions: torch.tensor of shape (n_atoms, 3). Contains the Cartesian
+        coordinates of the atoms. The implementation also works if the positions are
+        not contained within the unit cell.
+        :param charges: torch.tensor of shape (n_atoms, n_channels). In the simplest
+        case, this would be a tensor of shape (n_atoms, 1) where charges[i,0] is the
+        charge of atom i. More generally, the potential for the same atom positions
+        is computed for n_channels independent meshes, and one can specify the "charge"
+        of each atom on each of the meshes independently. For standard LODE that treats
+        all atomic species separately, one example could be:
+        If n_atoms = 4 and the species are [Na, Cl, Cl, Na], one could set n_channels=2
+        and use the one-hot encoding
+        charges = torch.tensor([[1,0],[0,1],[0,1],[1,0]])
+        for the charges. This would then separately compute the "Na" potential and
+        "Cl" potential. Subtracting these from each other, one could recover the more
+        standard electrostatic potential in which Na and Cl have charges of +1 and -1,
+        respectively.
+        :param subtract_self: bool. If set to true, the contribution to the potential of
+        the center atom itself is subtracted, meaning that only the potential generated
+        by the remaining atoms + periodic images of the center atom is taken into
+        account.
+
+        :returns: torch.tensor of shape (n_atoms, n_channels) containing the potential
+        at the position of each atom for the n_channels independent meshes separately.
+        """
+        smearing = self.parameters['atomic_gaussian_width']
+        mesh_resolution = self.parameters['mesh_spacing']
+        interpolation_order = self.parameters['interpolation_order']
+
+        # Initializations
+        n_atoms = len(positions)
+        assert positions.shape == (n_atoms,3)
+        assert charges.shape == (n_atoms, 1)
+
+        # Define k-vectors
+        if mesh_resolution is None:
+            k_cutoff = 2 * torch.pi / (smearing / 2)
+        else:
+            k_cutoff = 2 * torch.pi / mesh_resolution
+
+        # Compute number of times each basis vector of the
+        # reciprocal space can be scaled until the cutoff
+        # is reached
+        basis_norms = torch.linalg.norm(cell, axis=1)
+        ns_approx = k_cutoff * basis_norms / 2 / torch.pi
+        ns_actual_approx = 2 * ns_approx + 1 # actual number of mesh points
+        ns = 2**torch.ceil(torch.log2(ns_actual_approx)).long() # [nx, ny, nz]
+
+        # Step 1: Smear particles onto mesh
+        MI = MeshInterpolator(cell, ns, interpolation_order=interpolation_order)
+        MI.compute_interpolation_weights(positions)
+        rho_mesh = MI.points_to_mesh(particle_weights=charges)
+
+        # Step 2: Perform Fourier space convolution (FSC)
+        FSC = FourierSpaceConvolution(cell)
+        kernel_func = lambda ksq: 4*torch.pi / ksq * torch.exp(-0.5*smearing**2*ksq)
+        value_at_origin = 0. # charge neutrality
+        potential_mesh = FSC.compute(rho_mesh, kernel_func, value_at_origin)
+
+        # Step 3: Back interpolation
+        interpolated_potential = MI.mesh_to_points(potential_mesh)
+
+        # Remove self contribution
+        if subtract_self:
+            self_contrib = torch.sqrt(torch.tensor(2./torch.pi)) / smearing
+            for i in range(n_atoms):
+                interpolated_potential[i] -= charges[i,0] * self_contrib
+
+        return interpolated_potential
+
