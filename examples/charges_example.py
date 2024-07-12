6cdca3133291442c1ea93fab05d669c292957837
E-Rum <rev99@internet.ru>
2024-07-12 10:02:18 +0000

    Updated the example to resolve documentation and linting issues

--- examples/charges_example.py
+++ examples/charges_example.py
@@ -1,17 +1,16 @@
-# %% [markdown]
-# # Computations with Explicit Charges using Meshlode
+"""
+Computations with Explicit Charges using Meshlode
+=================================================
+In this tutorial, we will demonstrate how to perform
+computations with explicit charges using the Meshlode library. We will cover
+both the Torch and Metatensor branches of Meshlode.
 
-# %% [markdown]
-# In this tutorial, we will demonstrate how to perform
-# computations with explicit charges using the Meshlode library. We will cover
-# both the Torch and Metatensor branches of Meshlode.
-
-# %% [markdown]
-# ## Torch Version
-#
-# First, we will work with the Torch version of Meshlode. This involves using
-# PyTorch for tensor operations and ASE (Atomic Simulation Environment) for
-# creating and manipulating atomic structures.
+Torch Version
+--------------
+First, we will work with the Torch version of Meshlode. This involves using
+PyTorch for tensor operations and ASE (Atomic Simulation Environment) for
+creating and manipulating atomic structures.
+"""
 
 # %%
 import torch
@@ -24,10 +23,8 @@ from meshlode import PMEPotential
 from meshlode.metatensor import PMEPotential as MetaPMEPotential
 
 
-# %% [markdown]
-# ### Setting up the NaCl Unit Cell
-
 # %%
+# Setting up the NaCl Unit Cell
 # Define constants
 LATTICE_CONSTANT = 1.0
 CUTOFF = 1.0
@@ -45,23 +42,13 @@ atom_is, atom_js, neighbor_shifts = neighbor_list(
     "ijS", nacl, CUTOFF, self_interaction=False
 )
 
-# %% [markdown]
-# ### Initializing the PMEPotential Calculator
-
 # %%
-# Initialize the PMEPotential calculator with a specific exponent and short-range cutoff
-calculator = PMEPotential(
-    exponent=1.0,
-    sr_cutoff=CUTOFF,
-)
-
-# %% [markdown]
+# Initialize the PMEPotential calculator with a specific exponent.
 # The `PMEPotential` calculator is initialized with an exponent
 # and a short-range cutoff. This calculator will be used to compute the
 # potential energy of the system.
 
-# %% [markdown]
-# ### Computing the Potential with Single Channel Charges
+calculator = PMEPotential(exponent=1.0)
 
 # %%
 # Define the charges of the atoms as a 2D tensor, where each column
@@ -88,15 +75,8 @@ potential = calculator(
 # Print the calculated potential
 print(potential)
 
-# %% [markdown]
-# The charges of the atoms are defined in a tensor format and
-# passed to the `PMEPotential` calculator. The calculated potential is then
-# printed.
-
-# %% [markdown]
-# ### Computing the Potential with Multiple Channels for Charges
-
 # %%
+# Computing the Potential with Multiple Channels for Charges.
 # One can also use several channels for charges, e.g., for a system with 2
 # channels and 2 atoms as one-hot encoding
 charges = torch.tensor([[1.0, 0.0], [0.0, 1.0]], dtype=torch.float64)
@@ -121,23 +101,13 @@ potential = calculator(
 # Print the calculated potential
 print(potential)
 
-# %% [markdown]
-# We can also define charges with multiple channels, which allows
-# for more complex charge distributions. The calculated potential for this setup
-# is then printed.
-
-# %% [markdown]
-# ## Metatensor Version
-
-# %% [markdown]
+# %%
 # Next, we will work with the Metatensor version of Meshlode. This
 # involves using Metatensor for tensor operations while still utilizing ASE for
 # atomic structure creation.
 
-# %% [markdown]
-# ### Setting up the NaCl Unit Cell
+# Setting up the NaCl Unit Cell
 
-# %%
 # Define constants
 LATTICE_CONSTANT = 1.0
 CUTOFF = 1.0
@@ -154,26 +124,25 @@ nacl = Atoms(
 atom_is, atom_js, distances, neighbor_shifts = neighbor_list(
     "ijDS", nacl, CUTOFF, self_interaction=False
 )
-
-# %% [markdown]
-# ### Initializing the PMEPotential Calculator
-
 # %%
-# Initialize the PMEPotential calculator with a specific exponent and short-range cutoff
-calculator = MetaPMEPotential(
-    exponent=1.0,
-    sr_cutoff=CUTOFF,
-)
-
-# %% [markdown]
 # The `MetaPMEPotential` calculator is initialized with an
 # exponent and a short-range cutoff, similar to the Torch version.
 
-# %% [markdown]
-# ### Setting Up the System and Adding Charges
-
+calculator = MetaPMEPotential(exponent=1.0)
 # %%
-# Define the system with atomic types, positions, and cell dimensions
+
+# The system is defined using Metatensor's `System` class, which
+# includes atomic types, positions, and cell dimensions. We also create a
+# `NeighborList` object to store the neighbor indices, distances, and shifts.
+# The `NeighborListOptions` class is used to specify the cutoff distance and
+# whether to use the full neighbor list.
+#
+# NOTE: For our calculations, the parameter `types` passed to a `System` is
+# redundant; it will not be directly used to calculate the potential as the
+# potential depends only on the charge of the atom, not on the atom's type.
+# However, we still have to pass them because it is an obligatory parameter to
+# build the `System` class.
+
 system = System(
     types=torch.tensor(nacl.numbers),
     positions=torch.tensor(nacl.positions, dtype=torch.float64),
@@ -204,24 +173,12 @@ neighbor_list = TensorBlock(
 
 nl_options = NeighborListOptions(cutoff=CUTOFF, full_list=True)
 
-# %% [markdown]
-# The system is defined using Metatensor's `System` class, which
-# includes atomic types, positions, and cell dimensions. We also create a
-# `NeighborList` object to store the neighbor indices, distances, and shifts.
-# The `NeighborListOptions` class is used to specify the cutoff distance and
-# whether to use the full neighbor list.
-#
-# NOTE: For our calculations, the parameter `types` passed to a `System` is
-# redundant; it will not be directly used to calculate the potential as the
-# potential depends only on the charge of the atom, not on the atom's type.
-# However, we still have to pass them because it is an obligatory parameter to
-# build the `System` class.
-
-# %% [markdown]
-# ### Computing the Potential with Single Channel Charges
-
 # %%
-# Define the charges of the atoms
+# Computing the Potential with Single Channel Charges
+
+# The charges of the atoms are defined in a tensor format and
+# added to the system as a `TensorBlock`. The calculated potential is then
+# printed.
 charges = torch.tensor([[1.0], [-1.0]], dtype=torch.double)
 
 # Create a TensorBlock for the charges
@@ -242,13 +199,8 @@ potential = calculator.compute(system)
 # Print the calculated potential
 print(potential[0].values)
 
-# %% [markdown]
-# The charges of the atoms are defined in a tensor format and
-# added to the system as a `TensorBlock`. The calculated potential is then
-# printed.
-
-# %% [markdown]
-# ### Computing the Potential with Multiple Channels for Charges
+# %%
+# We can also define charges with multiple channels and add them to the system.
 
 # %%
 # Define the system again to reset
@@ -278,7 +230,4 @@ potential = calculator.compute(system)
 
 # Print the calculated potential
 print(potential[0].values)
-
-# %% [markdown]
-# We can also define charges with multiple channels and add them to the system.
 # %%
