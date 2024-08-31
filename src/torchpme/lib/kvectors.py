d622eabb2cd8fc74c88e48677a1f1e290bbb913a
Philip Loche <philip.loche@epfl.ch>
2024-08-31 11:34:18 +0200

    Reduce CUDA synchronizing operations

--- src/torchpme/lib/kvectors.py
+++ src/torchpme/lib/kvectors.py
@@ -1,6 +1,51 @@
 import torch
 
 
+def _generate_kvectors(
+    ns: torch.Tensor, cell: torch.Tensor, squeezed: bool
+) -> torch.Tensor:
+    # Check that all provided parameters have the correct shapes and are consistent
+    # with each other
+    if ns.shape != (3,):
+        raise ValueError(f"ns of shape {list(ns.shape)} should be of shape (3, )")
+
+    if cell.shape != (3, 3):
+        raise ValueError(f"cell of shape {list(cell.shape)} should be of shape (3, 3)")
+
+    if ns.device != cell.device:
+        raise ValueError(
+            f"`ns` and `cell` are not on the same device, got {ns.device} and "
+            f"{cell.device}."
+        )
+
+    if cell.is_cuda:
+        # use function that does not synchronize with the CPU
+        inverse_cell = torch.linalg.inv_ex(cell)[0]
+    else:
+        inverse_cell = torch.linalg.inv(cell)
+
+    reciprocal_cell = 2 * torch.pi * inverse_cell.T
+    bx = reciprocal_cell[0]
+    by = reciprocal_cell[1]
+    bz = reciprocal_cell[2]
+
+    # Generate all reciprocal space vectors from real FFT!
+    # The frequencies from the fftfreq function  are of the form [0, 1/n, 2/n, ...]
+    # These are then converted to [0, 1, 2, ...] by multiplying with n.
+    # get the frequencies, multiply with n, then w/ the reciprocal space vectors
+    kxs = (bx * ns[0]) * torch.fft.fftfreq(ns[0], device=ns.device).unsqueeze(-1)
+    kys = (by * ns[1]) * torch.fft.fftfreq(ns[1], device=ns.device).unsqueeze(-1)
+
+    if squeezed:
+        kzs = (bz * ns[2]) * torch.fft.fftfreq(ns[2], device=ns.device).unsqueeze(-1)
+    else:
+        kzs = (bz * ns[2]) * torch.fft.rfftfreq(ns[2], device=ns.device).unsqueeze(-1)
+
+    # then take the cartesian product (all possible combinations, same as meshgrid)
+    # via broadcasting (to avoid instantiating intermediates), and sum up
+    return kxs[:, None, None] + kys[None, :, None] + kzs[None, None, :]
+
+
 def generate_kvectors_for_mesh(ns: torch.Tensor, cell: torch.Tensor) -> torch.Tensor:
     """
     For a given unit cell, compute all reciprocal space vectors that are used to
@@ -21,42 +66,7 @@ def generate_kvectors_for_mesh(ns: torch.Tensor, cell: torch.Tensor) -> torch.Te
         space vectors that will be used in the (FFT-based) mesh calculators.
         Note that k_vectors[0,0,0] = [0,0,0] always is the zero vector.
     """
-    # Check that all provided parameters have the correct shapes and are consistent
-    # with each other
-    if ns.shape != (3,):
-        raise ValueError(f"ns of shape {list(ns.shape)} should be of shape (3, )")
-
-    if cell.shape != (3, 3):
-        raise ValueError(f"cell of shape {list(cell.shape)} should be of shape (3, 3)")
-
-    if ns.device != cell.device:
-        raise ValueError(
-            f"`ns` and `cell` are not on the same device, got {ns.device} and "
-            f"{cell.device}."
-        )
-
-    reciprocal_cell = 2 * torch.pi * cell.inverse().T
-    bx = reciprocal_cell[0]
-    by = reciprocal_cell[1]
-    bz = reciprocal_cell[2]
-
-    # Generate all reciprocal space vectors:
-    # The frequencies from the fftfreq function  are of the form [0, 1/n, 2/n, ...]
-    # These are then converted to [0, 1, 2, ...] by multiplying with n.
-    nx = int(ns[0])
-    ny = int(ns[1])
-    nz = int(ns[2])
-
-    # get the frequencies, multiply with n, then w/ the reciprocal space vectors
-    kxs = (bx * ns[0]) * torch.fft.fftfreq(nx, device=ns.device).unsqueeze(-1)
-    kys = (by * ns[1]) * torch.fft.fftfreq(ny, device=ns.device).unsqueeze(-1)
-    # real FFT!
-    kzs = (bz * ns[2]) * torch.fft.rfftfreq(nz, device=ns.device).unsqueeze(-1)
-    # then take the cartesian product (all possible combinations, same as meshgrid)
-    # via broadcasting (to avoid instantiating intermediates), and sum up
-    k_vectors = kxs[:, None, None] + kys[None, :, None] + kzs[None, None, :]
-
-    return k_vectors
+    return _generate_kvectors(ns=ns, cell=cell, squeezed=False)
 
 
 def generate_kvectors_squeezed(ns: torch.Tensor, cell: torch.Tensor) -> torch.Tensor:
@@ -82,33 +92,4 @@ def generate_kvectors_squeezed(ns: torch.Tensor, cell: torch.Tensor) -> torch.Te
         space vectors that will be used in the Ewald calculator.
         Note that k_vectors[0] = [0,0,0] always is the zero vector.
     """
-    # Check that all provided parameters have the correct shapes and are consistent
-    # with each other
-    if ns.shape != (3,):
-        raise ValueError(f"ns of shape {list(ns.shape)} should be of shape (3, )")
-
-    if cell.shape != (3, 3):
-        raise ValueError(f"cell of shape {list(cell.shape)} should be of shape (3, 3)")
-
-    if ns.device != cell.device:
-        raise ValueError(
-            f"`ns` and `cell` are not on the same device, got {ns.device} and "
-            f"{cell.device}."
-        )
-
-    reciprocal_cell = 2 * torch.pi * cell.inverse().T
-    bx = reciprocal_cell[0]
-    by = reciprocal_cell[1]
-    bz = reciprocal_cell[2]
-
-    # same as above, except for the z dimension (& the flattened output)
-    nx = int(ns[0])
-    ny = int(ns[1])
-    nz = int(ns[2])
-    kxs = (bx * ns[0]) * torch.fft.fftfreq(nx, device=ns.device).unsqueeze(-1)
-    kys = (by * ns[1]) * torch.fft.fftfreq(ny, device=ns.device).unsqueeze(-1)
-    kzs = (bz * ns[2]) * torch.fft.fftfreq(nz, device=ns.device).unsqueeze(-1)
-
-    k_vectors = kxs[:, None, None] + kys[None, :, None] + kzs[None, None, :]
-
-    return k_vectors.reshape(-1, 3)
+    return _generate_kvectors(ns=ns, cell=cell, squeezed=True).reshape(-1, 3)
