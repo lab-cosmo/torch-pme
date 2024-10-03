53ec3830310756469f4b52a6c222d735cf8e3b73
Michele Ceriotti <michele.ceriotti@gmail.com>
2024-10-03 15:18:21 +0200

    Fixed lots of errors, jitting the potential works

--- sandbox.py
+++ sandbox.py
@@ -5,7 +5,7 @@ import torchpme.lib.potential as potential
 import torch
 import matplotlib.pyplot as plt
 
-do_jit = False
+do_jit = True
 def jit(obj):
     return torch.jit.script(obj) if do_jit else obj
 
@@ -26,25 +26,6 @@ plt.plot(dist, sr_v, "b:", label="V_SR")
 plt.plot(dist, sr_v+lr_v, "k:", label="V_LR+V_SR")
 plt.legend()
 plt.show()
-
-# %%
-#  No cutoff
-
-lrpot = jit(potential.InversePowerLawPotential(exponent=1.0, range_radius=1.5, cutoff_radius=None))
-
-dist = torch.linspace(1,10,50)
-
-full_v = lrpot.from_dist(dist)
-lr_v = lrpot.lr_from_dist(dist)
-sr_v = lrpot.sr_from_dist(dist)
-
-plt.plot(dist, full_v, "gray", label="V")
-plt.plot(dist, lr_v, "r:", label="V_LR")
-plt.plot(dist, sr_v, "b:", label="V_SR")
-plt.plot(dist, sr_v+lr_v, "g:", label="V_LR+V_SR")
-plt.legend()
-plt.show()
-
 # %%
 # cutoff
 
@@ -62,4 +43,5 @@ plt.plot(dist, sr_v, "b:", label="V_SR")
 plt.plot(dist, sr_v+lr_v, "g:", label="V_LR+V_SR")
 plt.legend()
 plt.show()
+
 # %%
