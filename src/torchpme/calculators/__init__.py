dce11bd4e044b73e6e7e87b12ac44d82dd6fae89
Michele Ceriotti <michele.ceriotti@gmail.com>
2024-10-05 01:20:04 +0200

    Could run all examples (but not yet generate the webpages...)

--- src/torchpme/calculators/__init__.py
+++ src/torchpme/calculators/__init__.py
@@ -2,4 +2,10 @@ from .base import Calculator, estimate_smearing
 from .ewald import EwaldCalculator, tune_ewald
 from .pme import PMECalculator
 
-__all__ = ["Calculator", "EwaldCalculator", "PMECalculator", "tune_ewald"]
+__all__ = [
+    "Calculator",
+    "EwaldCalculator",
+    "PMECalculator",
+    "estimate_smearing",
+    "tune_ewald",
+]
