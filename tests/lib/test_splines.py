fdf719d2d1f497353b2f01b702359fa52f81ad38
Michele Ceriotti <michele.ceriotti@gmail.com>
2024-10-19 22:21:44 +0200

    Added tests for the splines

---
+++ tests/lib/test_splines.py
@@ -0,0 +1,25 @@
+import pytest
+import torch
+from torch.testing import assert_close
+
+from torchpme.lib.splines import (
+    CubicSpline, CubicSplineLongRange,
+    compute_spline_ft
+)
+
+@pytest.mark.parametrize("function", [torch.exp, torch.sin, torch.tanh])
+def test_spline_function(function):
+    x_grid = torch.linspace(-4,4,200)
+    x_test = torch.linspace(-torch.pi,torch.pi,23)
+    y_grid = function(x_grid)
+    y_test = function(x_test)
+
+    spline_test = CubicSpline(x_grid, y_grid)
+    z_grid = spline_test(x_grid)
+
+    # checks that the spline interpolates exactly on the grid
+    assert_close(y_grid, z_grid, atol=1e-15, rtol=0.0)
+
+    # checks that the spline is accurate-ish elsewhere
+    z_test = spline_test(x_test)
+    assert_close(y_test, z_test, atol=1e-6, rtol=0.0)
