d1421f4ca9677c90223a4044ef34020650ad9f8f
Michele Ceriotti <michele.ceriotti@gmail.com>
2024-11-05 11:25:28 +0100

    Added icon to docs

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -123,6 +123,8 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["../static"]
+html_favicon = "../static/torch-pme-icon.png"
+html_logo = "../static/torch-pme-icon.svg"
 
 # font-awesome logos (used in the footer)
 html_css_files = [
