3e35d74f163b558d8100b5bc69dc77500ae490c9
Michele Ceriotti <michele.ceriotti@gmail.com>
2024-11-24 01:08:24 +0100

    Nelder mead (doesn't work because actual error is not a good target)

--- examples/10-tuning.py
+++ examples/10-tuning.py
@@ -56,7 +56,7 @@ smearing = 0.5
 pme_params = { "mesh_spacing": 0.5, "interpolation_nodes": 4}
 cutoff = 5.0
 
-max_cutoff=20.0
+max_cutoff=32.0
 
 nl = vesin.NeighborList(cutoff=max_cutoff, full_list=False)
 i, j, S, d = nl.compute(
@@ -93,12 +93,12 @@ def timed_madelung(cutoff, smearing, mesh_spacing, interpolation_nodes):
     filter_indices = neighbor_indices[filter_idx]
     filter_distances = neighbor_distances[filter_idx]
 
-    start = time()
     pme = torchpme.PMECalculator(
         potential=torchpme.CoulombPotential(smearing=smearing), 
         mesh_spacing=mesh_spacing,
         interpolation_nodes=interpolation_nodes
     )
+    start = time()
     potential = pme(
         charges=charges,
         cell=cell,
@@ -150,4 +150,51 @@ cbar = fig.colorbar(contour, ax=ax[1], label="log10(time / s)")
 # a good heuristic is to keep cutoff/sigma constant (easy to 
 # determine error limit) to see how timings change
 
+smearing_grid = torch.logspace(-1, 0.5, 8)
+spacing_grid = torch.logspace(-1, 0.5, 9)
+results = np.zeros((len(smearing_grid), len(spacing_grid)))
+timings = np.zeros((len(smearing_grid), len(spacing_grid)))
+for ism, smearing in enumerate(smearing_grid):
+    for isp, spacing in enumerate(spacing_grid):
+        madelung, timing = timed_madelung(smearing*8, smearing, spacing, 4)
+        results[ism, isp] = madelung
+        timings[ism, isp] = timing
 
+
+# %%
+# plot
+
+fig, ax = plt.subplots(1,2,figsize=(7,3), constrained_layout=True)
+contour = ax[0].contourf(spacing_grid, smearing_grid, np.log10(np.abs(results-madelung_ref)))
+ax[0].set_xscale('log')
+ax[0].set_yscale('log')
+ax[0].set_ylabel(r"$\sigma$ / Å")
+ax[0].set_xlabel(r"spacing / Å")
+cbar = fig.colorbar(contour, ax=ax[0], label="log10(error)")
+
+contour = ax[1].contourf(spacing_grid, smearing_grid, np.log10(timings))
+ax[1].set_xscale('log')
+ax[1].set_yscale('log')
+ax[1].set_ylabel(r"$\sigma$ / Å")
+ax[1].set_xlabel(r"spacing / Å")
+cbar = fig.colorbar(contour, ax=ax[1], label="log10(time / s)")
+
+# %%
+
+from scipy.optimize import minimize
+
+def loss(x, target_accuracy):
+    cutoff, smearing, mesh_spacing = x
+    value, duration = timed_madelung(cutoff, smearing, mesh_spacing, 4)
+    tgt_loss = max(0,np.log(np.abs(value/madelung-1)/target_accuracy)) # relu on the accuracy    
+    print(x, np.abs(value/madelung-1)/target_accuracy, duration)
+    return tgt_loss*10 +  duration
+
+initial_guess = [8,0.3,5]
+result = minimize(loss, initial_guess, args=(1e-4), method='Nelder-Mead', options={'disp': True, 'maxiter': 200})
+
+
+# %%
+
+result
+# %%
