a332e78a129e649a65c0a8de59c4524fb1f3c063
Philip Loche <philip.loche@posteo.de>
2024-12-05 11:52:36 +0100

    enable macOS and Windows testsuite (#114)
    * enable macOS and Windows testsuite
    * split tests and min-tests

--- src/torchpme/utils/tuning/p3m.py
+++ src/torchpme/utils/tuning/p3m.py
@@ -121,7 +121,7 @@ def tune_p3m(
     Example
     -------
     >>> import torch
-    >>> from vesin.torch import NeighborList
+    >>> from vesin import NeighborList
     >>> _ = torch.manual_seed(0)
     >>> positions = torch.tensor(
     ...     [[0.0, 0.0, 0.0], [0.5, 0.5, 0.5]], dtype=torch.float64
