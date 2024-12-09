ac5c28fe66717165c3d41088d2bd110db613536a
Philip Loche <ploche@physik.fu-berlin.de>
2024-12-09 14:36:58 +0100

    Add skeleton for basic usage example

---
+++ examples/basic-usage.py
@@ -0,0 +1,26 @@
+"""
+Basic Usage
+===========
+
+.. currentmodule:: torchpme
+
+To follow this tutorial, it is assumed that torch-pme has been :ref:`installed
+<userdoc-installation>` on your computer.
+
+This is a basic usage example...
+"""
+
+# %%
+# sections:
+#
+# - Rattled and replicated nacl crystal
+# - Tune based on 5 Å cutoff
+# - Init a coulomb potential
+# - Compute NL
+# - Init P3M
+# - Compute potential energy
+# - Backprop to forces
+# - Do the same for nonperiodic
+# - Refer to :ref:`examples <userdoc-how-to>` for tackling specific and advanced tasks
+#   and to the :ref:`references <userdoc-reference>` for more information the spefific
+#   API.
