c4bb2f8a1dcb5c990b485273b8be90c55313f711
GardevoirX <xqj.2001@outlook.com>
2025-01-07 13:53:16 +0100

    Add tuning functions back

--- src/torchpme/utils/tuning/ewald.py
+++ src/torchpme/utils/tuning/ewald.py
@@ -115,3 +115,77 @@ class EwaldTuner(GridSearchBase):
         self.GridSearchParams["lr_wavelength"] *= float(
             torch.min(self._cell_dimensions)
         )
+
+
+def tune_ewald(
+    charges: torch.Tensor,
+    cell: torch.Tensor,
+    positions: torch.Tensor,
+    cutoff: float,
+    exponent: int = 1,
+    neighbor_indices: Optional[torch.Tensor] = None,
+    neighbor_distances: Optional[torch.Tensor] = None,
+    accuracy: float = 1e-3,
+):
+    r"""
+    Find the optimal parameters for :class:`torchpme.EwaldCalculator`.
+
+    The error formulas are given `online
+    <https://www2.icp.uni-stuttgart.de/~icp/mediawiki/images/4/4d/Script_Longrange_Interactions.pdf>`_
+    (now not available, need to be updated later). Note the difference notation between
+    the parameters in the reference and ours:
+
+    .. math::
+
+        \alpha &= \left( \sqrt{2}\,\mathrm{smearing} \right)^{-1}
+
+        K &= \frac{2 \pi}{\mathrm{lr\_wavelength}}
+
+        r_c &= \mathrm{cutoff}
+
+    :param charges: torch.Tensor, atomic (pseudo-)charges
+    :param cell: torch.Tensor, periodic supercell for the system
+    :param positions: torch.Tensor, Cartesian coordinates of the particles within
+        the supercell.
+    :param cutoff: float, cutoff distance for the neighborlist
+    :param exponent :math:`p` in :math:`1/r^p` potentials, currently only :math:`p=1` is
+        supported
+    :param neighbor_indices: torch.Tensor with the ``i,j`` indices of neighbors for
+        which the potential should be computed in real space.
+    :param neighbor_distances: torch.Tensor with the pair distances of the neighbors
+        for which the potential should be computed in real space.
+    :param accuracy: Recomended values for a balance between the accuracy and speed is
+        :math:`10^{-3}`. For more accurate results, use :math:`10^{-6}`.
+
+    :return: Tuple containing a float of the optimal smearing for the :class:
+        `CoulombPotential`, a dictionary with the parameters for
+        :class:`EwaldCalculator` and a float of the optimal cutoff value for the
+        neighborlist computation.
+
+    Example
+    -------
+    >>> import torch
+    >>> positions = torch.tensor(
+    ...     [[0.0, 0.0, 0.0], [0.5, 0.5, 0.5]], dtype=torch.float64
+    ... )
+    >>> charges = torch.tensor([[1.0], [-1.0]], dtype=torch.float64)
+    >>> cell = torch.eye(3, dtype=torch.float64)
+    >>> smearing, parameter, cutoff = tune_ewald(
+    ...     torch.sum(charges**2, dim=0), cell, positions, accuracy=1e-1
+    ... )
+
+    You can check the values of the parameters
+
+    >>> print(smearing)
+    1.7140874893066034
+
+    >>> print(parameter)
+    {'lr_wavelength': 0.25}
+
+    >>> print(cutoff)
+    4.4
+
+    """
+    return EwaldTuner(
+        charges, cell, positions, cutoff, exponent, neighbor_indices, neighbor_distances
+    ).tune(accuracy)
--- src/torchpme/utils/tuning/p3m.py
+++ src/torchpme/utils/tuning/p3m.py
@@ -185,3 +185,75 @@ class P3MTuner(GridSearchBase):
             neighbor_distances,
         )
         self.GridSearchParams["mesh_spacing"] *= float(torch.min(self._cell_dimensions))
+
+
+def tune_p3m(
+    charges: torch.Tensor,
+    cell: torch.Tensor,
+    positions: torch.Tensor,
+    cutoff: float,
+    exponent: int = 1,
+    neighbor_indices: Optional[torch.Tensor] = None,
+    neighbor_distances: Optional[torch.Tensor] = None,
+    accuracy: float = 1e-3,
+):
+    r"""
+    Find the optimal parameters for :class:`torchpme.calculators.pme.PMECalculator`.
+
+    For the error formulas are given `here <https://doi.org/10.1063/1.477415>`_.
+    Note the difference notation between the parameters in the reference and ours:
+
+    .. math::
+
+        \alpha = \left(\sqrt{2}\,\mathrm{smearing} \right)^{-1}
+
+    :param charges: torch.Tensor, atomic (pseudo-)charges
+    :param cell: torch.Tensor, periodic supercell for the system
+    :param positions: torch.Tensor, Cartesian coordinates of the particles within
+        the supercell.
+    :param cutoff: float, cutoff distance for the neighborlist
+    :param exponent :math:`p` in :math:`1/r^p` potentials, currently only :math:`p=1` is
+        supported
+    :param neighbor_indices: torch.Tensor with the ``i,j`` indices of neighbors for
+        which the potential should be computed in real space.
+    :param neighbor_distances: torch.Tensor with the pair distances of the neighbors
+        for which the potential should be computed in real space.
+    :param accuracy: Recomended values for a balance between the accuracy and speed is
+        :math:`10^{-3}`. For more accurate results, use :math:`10^{-6}`.
+
+    :return: Tuple containing a float of the optimal smearing for the :py:class:
+        `CoulombPotential`, a dictionary with the parameters for
+        :py:class:`PMECalculator` and a float of the optimal cutoff value for the
+        neighborlist computation.
+
+    Example
+    -------
+    >>> import torch
+
+    To allow reproducibility, we set the seed to a fixed value
+
+    >>> _ = torch.manual_seed(0)
+    >>> positions = torch.tensor(
+    ...     [[0.0, 0.0, 0.0], [0.5, 0.5, 0.5]], dtype=torch.float64
+    ... )
+    >>> charges = torch.tensor([[1.0], [-1.0]], dtype=torch.float64)
+    >>> cell = torch.eye(3, dtype=torch.float64)
+    >>> smearing, parameter, cutoff = tune_p3m(
+    ...     charges, cell, positions, cutoff=4.4, accuracy=1e-1
+    ... )
+
+    You can check the values of the parameters
+
+    >>> print(smearing)
+    1.7140874893066034
+
+    >>> print(parameter)
+    {'interpolation_nodes': 5, 'mesh_spacing': 0.2857142857142857}
+
+    >>> print(cutoff)
+    4.4
+
+    """
+    return P3MTuner(
+        charges, cell, positions, cutoff, exponent, neighbor_indices, neighbor_distances
+    ).tune(accuracy)
