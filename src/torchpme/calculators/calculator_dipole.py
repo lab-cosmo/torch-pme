Correct energy calculation in CalculatorDipole and fix smearing factor in PotentialDipole

src/torchpme/calculators/calculator_dipole.py
@@ -120,12 +120,13 @@ class CalculatorDipole(torch.nn.Module):
         # For this, we precompute trigonometric factors for optimization, which leads
         # to N^2 rather than N^3 scaling.
         trig_args = kvectors @ (positions.T)  # [k, i]
-        mu_k_product = dipoles @ kvectors.T  # [i, k]
         c = torch.cos(trig_args)  # [k, i]
         s = torch.sin(trig_args)  # [k, i]
         sc = torch.stack([c, s], dim=0)  # [2 "f", k, i]
-        sc_summed_G = torch.einsum("fki, ik, ic, k->fkc", sc, mu_k_product, dipoles, G)
-        energy = torch.einsum("fkc, ik ,fki->ic", sc_summed_G, mu_k_product, sc)
+        sc_summed_G = torch.einsum("fki, ic, k->fkc", sc, dipoles, G)
+        energy = torch.einsum(
+            "fkc, fki, kc, kc ->ic", sc_summed_G, sc, kvectors, kvectors
+        )
         energy /= torch.abs(cell.det())
 
         # Remove the self-contribution: Using the Coulomb potential as an

src/torchpme/potentials/potential_dipole.py
@@ -119,7 +119,7 @@ class PotentialDipole(torch.nn.Module):
                 "Cannot compute long-range contribution without specifying `smearing`."
             )
         alpha = 1 / (2 * self.smearing**2)
-        return 2 * torch.pi / 3 * torch.sqrt((alpha / torch.pi) ** 3)
+        return 4 * torch.pi / 3 * torch.sqrt((alpha / torch.pi) ** 3)
 
     def background_correction(self) -> torch.Tensor:
         if self.smearing is None: